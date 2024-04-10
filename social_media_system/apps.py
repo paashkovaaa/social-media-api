a261b606ad336601f8a17c944f029fa71d8d7437
Yuliana <pashkova.yuliana@gmail.com>
2024-04-10 16:52:05 +0300

    change from '' to ""

--- social_media_system/apps.py
+++ social_media_system/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SocialMedialSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'social_media_system'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "social_media_system"
