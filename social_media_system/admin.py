1b2a99841039e7e99719973f91231aa902458807
Yuliana <pashkova.yuliana@gmail.com>
2024-04-10 16:51:29 +0300

    register all models in admin panel

--- social_media_system/admin.py
+++ social_media_system/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media_system.models import Hashtag, Post, Like, Comment, Follow
+
+admin.site.register(Hashtag)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Follow)
